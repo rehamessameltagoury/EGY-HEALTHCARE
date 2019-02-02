8d8f486aa61d91145196004a4b816a0732ebbc84
rehamessameltagoury <36308055+rehamessameltagoury@users.noreply.github.com>
2019-02-02 22:16:44 +0200

    Add files via upload

---
+++ development/signup-login system/include/dbh_inc.php
@@ -0,0 +1,11 @@
+<?php
+//database connection
+
+$dbservername="localhost";
+$dbUsername="root";
+$dbPassword="";
+$dbName="Healthcare";
+$conn=mysqli_connect($dbservername,$dbUsername,$dbPassword,
+	$dbName);
+
+?>
---
+++ development/signup-login system/include/signup_inc.php
@@ -0,0 +1,64 @@
+<?php
+//he must click the button
+if (isset($_POST['submit'])) {
+	include_once 'dbh_inc.php';
+	$fname = mysqli_real_escape_string($conn,$_POST['fname']);
+	$lname = mysqli_real_escape_string($conn,$_POST['lname']);
+	$email = mysqli_real_escape_string($conn,$_POST['email']);
+	$user_name = mysqli_real_escape_string($conn,$_POST['user_name']);
+	$psw = mysqli_real_escape_string($conn,$_POST['psw']);
+	
+	$gender = $_POST['gender'];
+	//error handlers
+	//check for empty fields
+	if (empty($fname) || empty($lname) || empty($email)|| empty($user_name)||empty($psw) || empty($gender)) {
+		header("Location: ../signup.php?signup=empty");
+		exit();
+	}else{
+		//check if input characters are valid
+		if (!preg_match("/^[a-zA-Z]*$/", $fname) || !preg_match("/^[a-zA-Z]*$/", $lname)) {
+			header("Location: ../signup.php?signup=invalid");
+			exit();
+		} 
+
+		else{
+			// check if email is valid
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				header("Location: ../signup.php?signup=email");
+				exit();
+			} else{
+				//check user name is not repeated
+				$sql="SELECT * FROM USERS WHERE user_uid='$user_name'";
+				$result = mysqli_query($conn,$sql);
+				$resultCheck= mysqli_num_rows($result);
+
+				if ($resultCheck >0) {
+					header("Location: ../signup.php?signup=usertaken");
+				exit();
+				} else{
+					//hashing the password
+					$hashedpwd= password_hash($psw,PASSWORD_DEFAULT);
+					//insert user inside the database
+					$sql="INSERT INTO USERS (user_first,user_last,user_email,user_uid,user_pwd,user_gender) VALUES 
+					('$fname','$lname','$email','$user_name','$hashedpwd','$gender');";
+					mysqli_query($conn,$sql);
+					header("Location: ../profile.php?signup=signupsuccess");
+				exit();
+				}
+			
+		
+
+
+			}
+
+		}
+	}
+}
+else{
+	//if they didn't use the submit we will reload the page
+	header("Location: ../signup.php");
+	exit();
+}
+
+
+?>
