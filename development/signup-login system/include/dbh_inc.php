5ec555be52ee085c26bf7c0319fa62ff67c2dde9
israasyz <36207012+israasyz@users.noreply.github.com>
2019-02-11 21:26:36 +0200

    Update dbh_inc.php

--- development/signup-login system/include/dbh_inc.php
+++ development/signup-login system/include/dbh_inc.php
@@ -1,10 +1,10 @@
 <?php
 //database connection
 
-$dbservername="localhost";
-$dbUsername="root";
-$dbPassword="";
-$dbName="Healthcare";
+$dbservername="sql307.epizy.com";
+$dbUsername="epiz_23426192";
+$dbPassword="qrhufcVnYDEVx";
+$dbName="epiz_23426192_Healthcare";
 $conn=mysqli_connect($dbservername,$dbUsername,$dbPassword,
 	$dbName);
 
