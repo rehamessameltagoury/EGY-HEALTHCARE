1e55b085c7a8225534e37f721fbecacf070e31b5
MariamSafwat <36268243+MariamSafwat@users.noreply.github.com>
2019-02-14 21:53:07 +0200

    Update add_med_inc.php

--- development/Schedule/include/add_med_inc.php
+++ development/Schedule/include/add_med_inc.php
@@ -1,9 +1,13 @@
 <?php
 	include_once 'dbh_inc.php';
+	session_start();
 	$name = $_POST['name'];
     $time = $_POST['time'];
-    $sql = "INSERT INTO medicine (medicine_name,medicine_time) VALUES('$name','$time');";
+    $username=$_SESSION['username'];
+    $sql = "INSERT INTO medicine (medicine_name,medicine_time, user_name) VALUES('$name','$time', '$username');";
      mysqli_query($conn, $sql);
      
     header("location: ../Medicine_schedule.php?submit=success");
     ?> 
+
+    
