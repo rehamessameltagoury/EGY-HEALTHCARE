6f1100c8af63002076a4257f303b99d9fb5eff3d
MariamSafwat <36268243+MariamSafwat@users.noreply.github.com>
2019-02-14 21:52:37 +0200

    Update add_app_inc.php

--- development/Schedule/include/add_app_inc.php
+++ development/Schedule/include/add_app_inc.php
@@ -1,8 +1,10 @@
 <?php
 	include_once 'dbh_inc.php';
+	session_start();
 	$name = $_POST['name'];
     $time = $_POST['time'];
-    $sql = "INSERT INTO appointment (Dr_name,App_time) VALUES('$name','$time');";
+    $username=$_SESSION['username'];
+    $sql = "INSERT INTO appointment (Dr_name,App_time,user_name) VALUES('$name','$time','$username');";
      mysqli_query($conn, $sql);
      
     header("location: ../App_schedule.php?submit=success");
