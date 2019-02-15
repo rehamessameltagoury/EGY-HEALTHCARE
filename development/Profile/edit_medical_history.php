3eed8c7f53048193ae112e24af242fd2ffa19208
NadaGitHub <36456564+NadaGitHub@users.noreply.github.com>
2019-02-15 23:47:22 +0200

    Add files via upload

--- development/Profile/edit_medical_history.php
+++ development/Profile/edit_medical_history.php
@@ -22,6 +22,7 @@ a:active {
 #position_home { position: absolute; top: 57px; left: 680px; width: 200px ;font-size:25px}
 #position_about { position: absolute; top: 57px; left:880px; width: 200px ;font-size:25px}
 #position_sign { position: absolute; top: 57px; left: 1080px; width: 200px ;font-size:25px}
+#notifications { position: absolute; top: 57px; left: 1300px; width: 200px ;font-size:25px}
 body, html {
     height: 100%;
     margin: 0;
@@ -71,7 +72,8 @@ button:hover {
     opacity:1;
 }
 </style>
-
+<script>
+window.Engagespot={},q=function(e){return function(){(window.engageq=window.engageq||[]).push({f:e,a:arguments})}},f=["captureEvent","subscribe","init","showPrompt","identifyUser","clearUser"];for(k in f)Engagespot[f[k]]=q(f[k]);var s=document.createElement("script");s.type="text/javascript",s.async=!0,s.src="https://cdn.engagespot.co/EngagespotSDK.2.0.js";var x=document.getElementsByTagName("script")[0];x.parentNode.insertBefore(s,x);Engagespot.init('2pPMXEqgemY6QwNRguxpJBV7EdaImi');</script>
 </head>
 <body>
 
@@ -86,6 +88,8 @@ button:hover {
   <a id="position_about" href="about_us.html" target="_self">About us</a> 
   
   <a id="position_sign" href="profile.php" target="_self">Profile</a>
+
+  <a id="notifications"href="#"></a>
    
  </div>
    
