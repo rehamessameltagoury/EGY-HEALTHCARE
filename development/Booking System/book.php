c06df3bf7b99e096df44894b6ecc07ecf53a16c7
mariam-khaled <36280726+mariam-khaled@users.noreply.github.com>
2019-02-15 23:36:45 +0200

    Update book.php

--- development/Booking System/book.php
+++ development/Booking System/book.php
@@ -16,14 +16,97 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<style>
+a:link {
+    text-decoration: none;
+}
+
+a:visited {
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: none;
+}
+
+a:active {
+    text-decoration: none;
+}
+
+#logo { position: absolute; top: 0px; left: 200px; width: 200px }
+#position_home { position: absolute; top: 57px; left: 680px; width: 200px ;font-size:25px}
+#position_about { position: absolute; top: 57px; left:880px; width: 200px ;font-size:25px}
+#position_sign { position: absolute; top: 57px; left: 1080px; width: 200px ;font-size:25px}
+#notifications { position: absolute; top: 57px; left: 1300px; width: 200px ;font-size:25px}
+
+body, html {
+    height: 100%;
+    margin: 0;
+}
+
+
+.bg {
+
+    background-image: url("about us.jpg");
+
+    height: 100%; 
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+div.transbox {
+  margin: 30px;
+  background-color: #ffffff;
+  border: 1px solid white;
+  opacity: 0.6;
+  filter: alpha(opacity=60); /* For IE8 and earlier */
+  width : 600px;
+  height: 350px;
+}
+
+div.transbox p {
+  margin: 5%;
+  font-weight: bold;
+  font-size: 25px;
+  color: #000000;
+}
+
+</style>
+<script>
+window.Engagespot={},q=function(e){return function(){(window.engageq=window.engageq||[]).push({f:e,a:arguments})}},f=["captureEvent","subscribe","init","showPrompt","identifyUser","clearUser"];for(k in f)Engagespot[f[k]]=q(f[k]);var s=document.createElement("script");s.type="text/javascript",s.async=!0,s.src="https://cdn.engagespot.co/EngagespotSDK.2.0.js";var x=document.getElementsByTagName("script")[0];x.parentNode.insertBefore(s,x);Engagespot.init('2pPMXEqgemY6QwNRguxpJBV7EdaImi');</script>
 </head>
+
+
+<body>
+
+<div style="background-color:white;color:brown;padding:20px;height:160px">
+
+  <div id="logo">
+  <img src="logo.jpg" alt="logo">
+  </div>
+  
+ <a id="position_home" href="index1.html" target="_self">Home</a> 
+  
+  <a id="position_about" href="about_us.html" target="_self">About us</a> 
+  
+  <a id="position_sign" href="profile.php" target="_self">Profile</a>
+
+   <a id="notifications"href="#"></a>
+   
+ </div>
+
+</body>
 
 </html>
 
 
 <?php
 
-session_start();
+//session_start();
 
 global $con ;
 $con= mysqli_connect('sql307.epizy.com','epiz_23426192','qrhufcVnYDEVx');
@@ -36,7 +119,7 @@ if (! mysqli_select_db($con,'epiz_23426192_Healthcare')) {
 	echo "Database not selected";
 }
 	
-$name=$_SESSION['username'];	
+//$name=$_SESSION['username'];	
 
 $patient=$_POST['full_name'];
 $email=$_POST['e_mail'];
@@ -45,9 +128,12 @@ $depp=$_POST['dr_dep'];
 $time=$_POST['day'];
 
 //check that the doctor and appointment day entered by user exist
-$sql1= "SELECT name,department FROM doctors WHERE name = '$dr' AND department='$depp' AND appointments LIKE ('%$time%') ,
+$sql1= "SELECT name,department FROM doctors WHERE name = '$dr' AND department='$depp' AND appointments LIKE ('%$time%')";
+
+/*$sql1= "SELECT name,department FROM doctors WHERE name = '$dr' AND department='$depp' AND appointments LIKE ('%$time%') ,
     JOIN patients_appointments ON patients_appointments.doctor = doctors.name ,
-    JOIN users ON users.user_email = patients_appointments.email where users.user_name = '".$name."'";
+    JOIN users ON users.user_email = patients_appointments.email where users.user_name = '".$name."'"; */
+
 $result=mysqli_query($con,$sql1);
 
 //executed when info. entered by user is valid
