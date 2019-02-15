91d735ada235f26f335739574c8a5496bf9d6cf4
NadaGitHub <36456564+NadaGitHub@users.noreply.github.com>
2019-02-15 23:01:43 +0200

    Add files via upload

---
+++ development/notifications.php
@@ -0,0 +1,6 @@
+#notifications { position: absolute; top: 57px; left: 1300px; width: 200px ;font-size:25px}
+
+<script>
+window.Engagespot={},q=function(e){return function(){(window.engageq=window.engageq||[]).push({f:e,a:arguments})}},f=["captureEvent","subscribe","init","showPrompt","identifyUser","clearUser"];for(k in f)Engagespot[f[k]]=q(f[k]);var s=document.createElement("script");s.type="text/javascript",s.async=!0,s.src="https://cdn.engagespot.co/EngagespotSDK.2.0.js";var x=document.getElementsByTagName("script")[0];x.parentNode.insertBefore(s,x);Engagespot.init('2pPMXEqgemY6QwNRguxpJBV7EdaImi');</script>
+
+<a id="notifications"href="#"></a>
