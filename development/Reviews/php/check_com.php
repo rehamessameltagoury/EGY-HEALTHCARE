5804681c171ebd413521847b0694a785ba24b7a5
israasyz <36207012+israasyz@users.noreply.github.com>
2019-02-15 20:05:02 +0200

    Update check_com.php

--- development/Reviews/php/check_com.php
+++ development/Reviews/php/check_com.php
@@ -5,8 +5,9 @@
 		$new_com_text = $_POST['comment'];
 		$new_com_date = date('Y-m-d H:i:s');
 		$new_com_code = generateRandomString();
+		$new_com_docname=$_GET['review'];
 		if(isset($new_com_text)) {
-			mysqli_query($connect, "INSERT INTO `parents` (`username`, `text`, `date`, `code`) VALUES ('$new_com_name', '$new_com_text', '$new_com_date', '$new_com_code')");
+			mysqli_query($connect, "INSERT INTO `parents` (`username`, `text`, `date`, `code` , `docname`) VALUES ('$new_com_name', '$new_com_text', '$new_com_date', '$new_com_code' , '$new_com_docname')");
 		}
 		header("Location: ");
 	}
@@ -16,8 +17,9 @@
 		$new_reply_text = $_POST['new-reply'];
 		$new_reply_date = date('Y-m-d H:i:s');
 		$new_reply_code = $_POST['code'];
+		$new_reply_docname=$_GET['review'];
 		if(isset($new_reply_text)) {
-			mysqli_query($connect, "INSERT INTO `children` (`username`, `text`, `date`, `par_code`) VALUES ('$new_reply_name', '$new_reply_text', '$new_reply_date', '$new_reply_code')") or die(mysqli_error($connect));
+			mysqli_query($connect, "INSERT INTO `children` (`username`, `text`, `date`, `par_code`, `docname`) VALUES ('$new_reply_name', '$new_reply_text', '$new_reply_date', '$new_reply_code','$new_reply_docname')") or die(mysqli_error($connect));
 		}
 		header("Location: ");
 	}
