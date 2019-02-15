d7f71ff19629cc775ca24a3713c26ce1e7b4d42a
israasyz <36207012+israasyz@users.noreply.github.com>
2019-02-15 16:21:17 +0200

    Add files via upload

---
+++ development/Reviews/comment.php
@@ -0,0 +1,139 @@
+<?php require_once 'php/connect.php'; require_once 'php/functions.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	    <title>EGY HEALTHCARE</title>
+        <meta charset="UTF-8" lang="en-US">
+        <link rel="stylesheet" href="style.css">
+		<script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+		<script src="js/global.js"></script>	
+
+<style>
+a:link {
+    text-decoration: none;
+}
+a:visited {
+    text-decoration: none;
+}
+a:hover {
+    text-decoration: none;
+}
+a:active {
+    text-decoration: none;
+}
+#logo { position: absolute; top: 0px; left: 200px; width: 200px }
+#position_home { position: absolute; top: 43px; left: 680px; width: 200px ;font-size:25px}
+#position_about { position: absolute; top: 43px; left:880px; width: 200px ;font-size:25px}
+#position_sign { position: absolute; top: 43px; left: 1080px; width: 200px ;font-size:25px}
+body, html {
+    height: 100%;
+    margin: 0;
+}
+.bg {
+    background-image: url("form.jpg");
+    height: 100%;  width: 50%;
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box}
+/* Full-width input fields */
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 15px;
+    margin: 5px 0 22px 0;
+    display: inline-block;
+    border: none;
+    background: #f1f1f1;
+}
+input[type=text]:focus, input[type=password]:focus {
+    background-color: #ddd;
+    outline: none;
+  width:200px;
+}
+hr {
+    border: 1px solid #f1f1f1;
+    margin-bottom: 25px;
+}
+/* Set a style for all buttons */
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+    opacity: 0.9;
+}
+button:hover {
+    opacity:1;
+}
+/* Extra styles for the cancel button */
+.cancelbtn {
+    padding: 14px 20px;
+    background-color: #f44336;
+}
+/* Float cancel and signup buttons and add an equal width */
+.cancelbtn, .signupbtn {
+  float: left;
+  width: 50%;
+}
+/* Add padding to container elements */
+.container {
+    padding: 16px;
+}
+/* Clear floats */
+.clearfix::after {
+    content: "";
+    clear: both;
+    display: table;
+}
+/* Change styles for cancel button and signup button on extra small screens */
+@media screen and (max-width: 300px) {
+    .cancelbtn, .signupbtn {
+       width: 100%;
+    }
+}
+#sign { position: absolute; left: 780px; height=100%}
+</style>
+
+</head>
+<body>
+
+<div style="background-color:white;color:brown;padding:20px;height:120px">
+
+  <div id="logo">
+  <img src="logo.jpg" alt="logo" height="121.5">
+  </div>
+  
+ <a id="position_home" href="home.html" target="_self">Home</a> 
+  
+  <a id="position_about" href="about_us.html" target="_self">About us</a> 
+  
+  <a id="position_sign" href="signup.php" target="_self">Sign up/Login</a>
+   
+ </div>
+
+	</head>
+	<body>
+		<div class="page-container">
+			<?php
+             
+             $text=$_POST["review"];
+				get_total($text); //get number of total comments written
+				require_once 'php/check_com.php';
+			?>
+			<form action="" method="post" class="main">
+				<label>enter a brief comment</label>
+				<textarea class="form-text" name="comment" id="comment"></textarea>
+				<br />
+				<input type="submit" class="form-submit" name="new_comment" value="post">
+			</form>
+			<?php get_comments($text); //get comments on screen
+			?> 
+		</div>
+	</body>
+</html>
