e48432fd51c0faa69efbd85193a21d91f20d570b
MariamSafwat <36268243+MariamSafwat@users.noreply.github.com>
2019-02-15 13:48:57 +0200

    Add files via upload

---
+++ development/Profile/include/dbh_inc.php
@@ -0,0 +1,10 @@
+<?php
+//database connection
+$dbservername="localhost";
+$dbUsername="root";
+$dbPassword="";
+$dbName="Healthcare";
+$conn=mysqli_connect($dbservername,$dbUsername,$dbPassword,
+	$dbName);
+
+?>
